78a302497a9f0eda4e5c70d43ddca752fdf1264d
Emily Caples <etcaples@gmail.com>
2018-06-12 11:42:55 -0700

    sorts reviews by date, rating, and by 'featured'

--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection(mysqlConfig);
 // load helper functions for database access into this folder
 
 const getReviews = (callback) => {
-  connection.query('SELECT * FROM ((reviews INNER JOIN users ON reviews.user_id = users.user_id) INNER JOIN photos ON reviews.photos_id = photos.photos_id) WHERE dress_id = 1', (err, results) => {
+  connection.query('SELECT * FROM ((reviews INNER JOIN users ON reviews.user_id = users.user_id) INNER JOIN photos ON reviews.photos_id = photos.photos_id) WHERE dress_id = 21', (err, results) => {
       if (err) {
         callback(err, null);
       } else {
