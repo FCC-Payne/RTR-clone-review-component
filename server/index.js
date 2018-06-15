de34e86c5e63939561d481043ca6c438de4b34ad
Emily Caples <etcaples@gmail.com>
2018-06-15 12:10:53 -0700

    refactors server to run independently as well as receive requests from a proxy server

--- server/index.js
+++ server/index.js
@@ -5,17 +5,23 @@ const db = require('../database/index.js');
 
 const app = express();
 
+const options = {
+  setHeaders: (res) => {
+    res.set({ 'Access-Control-Allow-Origin': 'http://localhost:3000' });
+  },
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
-app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/:id', express.static(path.join(__dirname, '/../client/dist'), options));
 
 app.get('/:id/reviews', (req, res) => {
   db.getReviews(req.params.id, (err, data) => {
     if (err) {
       res.status(500).send('ERROR FETCHING FROM DATABASE', err);
     } else {
-      res.status(200).send(JSON.stringify(data));
+      res.status(200).set({ 'Access-Control-Allow-Origin': 'http://localhost:3000' }).send(JSON.stringify(data));
     }
   });
 });
