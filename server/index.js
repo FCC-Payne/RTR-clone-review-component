1676c8f08e37f0228602c008edba49d23a0c4c8d
Emily Caples <etcaples@gmail.com>
2018-06-19 10:23:27 -0700

    adjusted dependicies list; refactored server to clean up code and make it work with localhost

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const db = require('../database/index.js');
 
-// const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3002;
 
 const app = express();
 
@@ -13,7 +13,7 @@ const options = {
     res.set({ 'Access-Control-Allow-Origin': '*' });
   },
 };
-// app.use(req => console.log('HIT:', req.url));
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,13 +23,11 @@ app.use('/:id', express.static(path.join(__dirname, '/../client/dist'), options)
 app.get('/:id/reviews', (req, res) => {
   db.getReviews(req.params.id, (err, data) => {
     if (err) {
-      console.log('ERROR FETCHING FROM DATABASE');
       res.status(500).send(err);
     } else {
-      console.log('SUCCESS FETCHING FROM DATABASE');
       res.status(200).set({ 'Access-Control-Allow-Origin': '*' }).send(data);
     }
   });
 });
 
-app.listen(8081, () => console.log('listening on port 8081'));
+app.listen(port, () => console.log(`listening on port ${port}`));
